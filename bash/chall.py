47ef5e73668709063f72458c1a4fe0fdb2965972
kashew <katherinec1488@gmail.com>
2023-04-10 19:17:09 -0700

    t_t

--- bash/chall.py
+++ bash/chall.py
@@ -1,13 +1,15 @@
 #!/usr/local/bin/python
+#flag{b4sh_1n_pyth0n}
 from os import system
+
 inp = input("What's your first string?\n")
 rep = '/' + input("What should you replace?\n") +  '/' 
-print(rep)
 new = input("What should it be now?\n")
 cmd = 's' + rep + new + "/g"
-print(cmd)
 if "'" not in new:
 	cmd += "'"
 print("You actually said")
 
 system("echo '" + inp + "' | sed '" +  cmd)
+
+# '; cat flag.txt; #
